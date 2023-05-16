38cab4cdddb1ea4b943af813b09c5efec58a162c
Hidetaka Saruhashi <saruhashi@nhkcma.com>
2023-05-16 14:12:41 -0400

    プロジェクト ファイルを追加します。

---
+++ test_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("test_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("test_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("E8647FE0-F4A0-4245-B970-1173347DFF79")]
+[assembly: AssemblyVersion("1.0.0.0")]
